6eabaedf594d1f7e29dc94b64f0dd82b6fef1625
zlb <libao.zheng@tongdun.cn>
2020-08-21 10:48:42 +0800

    no message

--- src/main/java/cn/zlb/redis/RedisController.java
+++ src/main/java/cn/zlb/redis/RedisController.java
@@ -1,5 +1,6 @@
 package cn.zlb.redis;
 
+import cn.zlb.biz.domain.UserDO;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +18,23 @@ public class RedisController {
     @Resource
     private RedisTemplate<String, Object> redisTemplate;
 
-    @GetMapping("testRedis")
-    public Object testRedis() {
+    /**
+     * 存储字符串
+     */
+    @GetMapping("testRedis2")
+    public Object testRedis2() {
         String key = "name";
         redisTemplate.opsForValue().set(key, "test");
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    /**
+     * 存储对象
+     */
+    @GetMapping("testRedis")
+    public Object testRedis() {
+        String key = "user1";
+        redisTemplate.opsForValue().set(key, new UserDO(1L, "张三", "10086", "zhangsan@163.com"));
         return redisTemplate.opsForValue().get(key);
     }
 }
