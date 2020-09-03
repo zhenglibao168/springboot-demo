526d221fe357fb504d5388405ef9a0e55780347e
zlb <libao.zheng@tongdun.cn>
2020-09-03 18:47:32 +0800

    11

--- src/test/java/cn/zlb/MyTest.java
+++ src/test/java/cn/zlb/MyTest.java
@@ -13,17 +13,17 @@ import java.util.ArrayList;
 public class MyTest {
     @Test
     public void test() {
-        final ArrayList<Integer> list1 = new ArrayList<>();
-        final int initialCapacity = 1000000;
-        final ArrayList<Integer> list2 = new ArrayList<>(initialCapacity);
-        final long begin1 = System.currentTimeMillis();
+        ArrayList<Integer> list1 = new ArrayList<>();
+        int initialCapacity = 1000000;
+        ArrayList<Integer> list2 = new ArrayList<>(initialCapacity);
+        long begin1 = System.currentTimeMillis();
         for (int i = 0; i < initialCapacity; i++) {
             list1.add(1);
         }
         System.out.println("list1 cost time: " + (System.currentTimeMillis() - begin1));
 
 
-        final long begin2 = System.currentTimeMillis();
+        long begin2 = System.currentTimeMillis();
         for (int i = 0; i < initialCapacity; i++) {
             list2.add(1);
         }
