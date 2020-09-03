a53e75844c520057a3b9f6b1055754999fb33db3
zlb <libao.zheng@tongdun.cn>
2020-09-03 18:20:34 +0800

    11

--- src/test/java/cn/zlb/MyTest.java
+++ src/test/java/cn/zlb/MyTest.java
@@ -2,6 +2,8 @@ package cn.zlb;
 
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+
 /**
  * 测试类
  *
@@ -11,8 +13,20 @@ import org.testng.annotations.Test;
 public class MyTest {
     @Test
     public void test() {
-        Long a = 128L;
-        Long b = 128L;
-        System.out.println(a == b);//false
+        final ArrayList<Integer> list1 = new ArrayList<>();
+        final int initialCapacity = 1000000;
+        final ArrayList<Integer> list2 = new ArrayList<>(initialCapacity);
+        final long begin1 = System.currentTimeMillis();
+        for (int i = 0; i < initialCapacity; i++) {
+            list1.add(1);
+        }
+        System.out.println("list1 cost time: " + (System.currentTimeMillis() - begin1));
+
+
+        final long begin2 = System.currentTimeMillis();
+        for (int i = 0; i < initialCapacity; i++) {
+            list2.add(1);
+        }
+        System.out.println("list2 cost time: " + (System.currentTimeMillis() - begin2));
     }
 }
