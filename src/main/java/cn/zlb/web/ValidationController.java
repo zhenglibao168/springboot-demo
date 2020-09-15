b219541ecadd64f8f43d49189690dc87a734afd8
zlb <libao.zheng@tongdun.cn>
2020-09-15 14:55:30 +0800

    fixbug

--- src/main/java/cn/zlb/web/ValidationController.java
+++ src/main/java/cn/zlb/web/ValidationController.java
@@ -8,7 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 
 /**
- * controller加上@Validated注解且controller方法是private时，controller依赖注入ValidationService值为null
+ * 1、IOC容器存放的bean是ValidationController的cglib代理类，beanName为validationController,不会存储ValidationController的bean
+ * 2、cglib只能代理非private非final方法，通过回调 实现方法调用
+ * 3、cglib不能代理private或final方法，通过反射 实现方法调用
+ * 4、testPublic方法调用通过FastClass回调ValidationController对象，ValidationController对象是在bean初始化之后放在代理对象的callback里
+ * 5、testPrivate方法调用通过反射调用ValidationController代理对象，报空指针异常
+ * CglibAopProxy.getProxy方法调用ObjenesisCglibAopProxy.createProxyClassAndInstance方法
+ * createProxyClassAndInstance方法调用SpringObjenesis.newInstance方法生成代理类对象ValidationController$$EnhancerBySpringCGLIB$$97f55215@54c1e5ca
+ * 该对象属性ValidationService为null，所以执行validationService.test()报空指针异常
+ * 如何避免？把testPrivate方法的访问修饰符private改成public、protected其中一个或去掉访问修饰符private
  *
  * @author libao.zheng
  * @date 2020/9/11 9:51 上午
