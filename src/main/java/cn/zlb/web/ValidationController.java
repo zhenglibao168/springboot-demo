2a0a4ef0df75f7bca7473a93558d6da8c0f1773f
zlb <libao.zheng@tongdun.cn>
2020-09-15 15:10:57 +0800

    no message

--- src/main/java/cn/zlb/web/ValidationController.java
+++ src/main/java/cn/zlb/web/ValidationController.java
@@ -10,7 +10,7 @@ import javax.annotation.Resource;
 /**
  * 1、IOC容器存放的bean是ValidationController的cglib代理类，beanName为validationController,不会存储ValidationController的bean
  * 2、cglib只能代理非private非final方法，通过回调 实现方法调用
- * 3、cglib不能代理private或final方法，通过反射 实现方法调用
+ * 3、cglib不能代理private或final方法，通过反射 实现方法调用，详见VisibilityPredicate类evaluate方法
  * 4、testPublic方法调用通过FastClass回调ValidationController对象，ValidationController对象是在bean初始化之后放在代理对象的callback里
  * 5、testPrivate方法调用通过反射调用ValidationController代理对象，报空指针异常
  * CglibAopProxy.getProxy方法调用ObjenesisCglibAopProxy.createProxyClassAndInstance方法
