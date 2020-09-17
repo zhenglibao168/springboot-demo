c581720d5a6f80b48fe12fedb360dfe19c2d886b
zlb <libao.zheng@tongdun.cn>
2020-09-17 17:33:09 +0800

    no message

--- src/main/java/cn/zlb/biz/spring/event/MyContextListener.java
+++ src/main/java/cn/zlb/biz/spring/event/MyContextListener.java
@@ -8,6 +8,7 @@ import javax.annotation.Resource;
 
 /**
  * 自定义监听器
+ * 观察者模式
  *
  * @author libao.zheng
  * @date 2020/7/17 1:44 下午
@@ -24,7 +25,7 @@ public class MyContextListener implements ApplicationListener<MyContextEvent> {
 
     @Override
     public void onApplicationEvent(MyContextEvent event) {
-      // System.out.println("自定义事件监听器MyContextListener,事件发布来源：" + event.getPublishSource());
-      // System.out.println(event);
+        // System.out.println("自定义事件监听器MyContextListener,事件发布来源：" + event.getPublishSource());
+        // System.out.println(event);
     }
 }
