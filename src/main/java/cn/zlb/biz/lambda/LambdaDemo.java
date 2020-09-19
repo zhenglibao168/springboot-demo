acb3b3ce9f5f30126b40521777fcf2004d6e7400
zlb <libao.zheng@tongdun.cn>
2020-09-19 13:45:12 +0800

    no message

--- src/main/java/cn/zlb/biz/lambda/LambdaDemo.java
+++ src/main/java/cn/zlb/biz/lambda/LambdaDemo.java
@@ -1,5 +1,8 @@
 package cn.zlb.biz.lambda;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -11,7 +14,7 @@ import java.util.stream.Stream;
  * @date 2020/7/8 3:31 下午
  */
 public class LambdaDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
 //************************* Map相关操作 *************************//
         HashMap<String, Integer> map = new HashMap<>();
@@ -43,7 +46,7 @@ public class LambdaDemo {
         });
 
         //遍历map所有元素
-      // System.out.println("map 遍历所有元素：");
+        // System.out.println("map 遍历所有元素：");
         map.forEach((key, value) -> System.out.println(key + ":" + value));
 
         //过滤map中符合条件的元素
@@ -53,7 +56,7 @@ public class LambdaDemo {
             }
             return true;
         });
-      // System.out.println("map 过滤后结果：");
+        // System.out.println("map 过滤后结果：");
         entryStream.forEach(entry -> System.out.println(entry.getKey() + ":" + entry.getValue()));
 
 
@@ -74,7 +77,7 @@ public class LambdaDemo {
             newTreeMap.put(entry.getKey(), entry.getValue());
         }
 
-      // System.out.println("treeMap按key降序排序之后结果：");
+        // System.out.println("treeMap按key降序排序之后结果：");
         newTreeMap.forEach((key, value) -> System.out.println(key + ":" + value));
 
         //TreeMap/LinkedHashMap按value升序排序
@@ -94,7 +97,7 @@ public class LambdaDemo {
             newTreeMap2.put(entry.getKey(), entry.getValue());
         }
 
-      // System.out.println("treeMap按value升序排序之后结果：");
+        // System.out.println("treeMap按value升序排序之后结果：");
         newTreeMap2.forEach((key, value) -> System.out.println(key + ":" + value));
 
 
@@ -113,7 +116,7 @@ public class LambdaDemo {
         });
 
         //遍历list所有元素
-      // System.out.println("list 遍历所有元素:");
+        // System.out.println("data 遍历所有元素:");
         list.forEach(item -> System.out.println("item=" + item));
 
         //过滤list中符合条件的元素，生成新list
@@ -123,7 +126,7 @@ public class LambdaDemo {
             }
             return false;
         }).collect(Collectors.toList());
-      // System.out.println("list 过滤后结果：");
+        // System.out.println("data 过滤后结果：");
         collect.forEach(item -> System.out.println("item=" + item));
 
         //list基本类型排序
@@ -134,11 +137,11 @@ public class LambdaDemo {
         list2.sort(new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
-              // System.out.println("list2 比较元素大小:" + o1 + " vs " + o2);
+                // System.out.println("list2 比较元素大小:" + o1 + " vs " + o2);
                 return o1.compareTo(o2);
             }
         });
-      // System.out.println("list2 按升序排序结果：" + list2);
+        // System.out.println("list2 按升序排序结果：" + list2);
 
 
         //list对象类型排序
@@ -153,7 +156,7 @@ public class LambdaDemo {
                 return o1.getAge().compareTo(o2.getAge());
             }
         });
-      // System.out.println("students按age升序排序后：" + students);
+        // System.out.println("students按age升序排序后：" + students);
 
 
         students.sort(new Comparator<Student>() {
@@ -162,7 +165,7 @@ public class LambdaDemo {
                 return o2.getAge().compareTo(o1.getAge());
             }
         });
-      // System.out.println("students按age降序排序后：" + students);
+        // System.out.println("students按age降序排序后：" + students);
 
         //List转数组
         ArrayList<String> sourceList = new ArrayList<>();
@@ -172,9 +175,9 @@ public class LambdaDemo {
         sourceList.add("555");
 
         String[] descArray = sourceList.toArray(new String[0]);
-      // System.out.println("List转数组后:");
+        // System.out.println("List转数组后:");
         for (String s : descArray) {
-          // System.out.println(s);
+            // System.out.println(s);
         }
 
 
@@ -187,7 +190,52 @@ public class LambdaDemo {
             }
         });
         List<String> sortedList = list.stream().distinct().collect(Collectors.toList());
-      // System.out.println("List排序并去重:");
+        // System.out.println("List排序并去重:");
         list.forEach(System.out::println);
+
+        //************************* 数组相关操作 *************************//
+
+        //串行流
+        String[] arr = new String[]{"json", "json", "jerry", "army", "lucy", "james"};
+        Stream.of(arr)
+                .filter(s -> s.startsWith("j"))//过滤符合条件的元素，不改变原有数组
+                .map(String::toUpperCase)//元素操作
+                .sorted()//按首字母升序排序
+                .distinct()//去重
+                .collect(Collectors.toList())//生成list
+                .forEach(System.out::println);
+
+        //并行流：适合多核CPU，性能高于for循环，for循环高于串行流
+        Stream.of(arr).parallel().filter(s -> s.startsWith("j"))//过滤符合条件的元素，不改变原有数组
+                .map(String::toUpperCase)//元素操作
+                .sorted()//按首字母升序排序
+                .distinct()//去重
+                .collect(Collectors.toList())//生成list
+                .forEach(System.out::println);
+
+        List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
+        //当流中每个元素都符合该断言时才返回true，否则返回false
+        boolean allMatch = data.stream().allMatch(e -> e > 10); //false
+        //当流中每个元素都不符合该断言时才返回true，否则返回false
+        boolean noneMatch = data.stream().noneMatch(e -> e > 10); //true
+        //只要流中有一个元素满足该断言则返回true，否则返回false
+        boolean anyMatch = data.stream().anyMatch(e -> e > 4); //true
+        //返回流中第一个元素
+        Integer findFirst = data.stream().findFirst().get(); //1
+        //返回流中的任意元素
+        Integer findAny = data.stream().findAny().get(); //1
+        //返回流中元素的总个数
+        long count = data.stream().count(); //5
+        //返回流中元素最大值
+        Integer max = data.stream().max(Integer::compareTo).get(); //5
+        //返回流中元素最小值
+        Integer min = data.stream().min(Integer::compareTo).get(); //1　
+
+        //使用 BufferedReader.lines() 方法，将每行内容转成流
+        BufferedReader reader = new BufferedReader(new FileReader("F:\\test_stream.txt"));
+        Stream<String> lineStream = reader.lines();
+        lineStream.forEach(System.out::println);
+
     }
+
 }
